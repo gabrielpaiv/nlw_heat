d4ab001a2a46d53190b9ac86036b4ed812c299c7
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-10-21 20:16:42 -0300

    Added Send Message Form component

---
+++ nlwheatapp/src/components/SendMessageForm/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+import { TextInput, View } from 'react-native'
+import { COLORS } from '../../theme'
+import { Button } from '../Button'
+
+import { styles } from './styles'
+
+export function SendMessageForm() {
+  const [message, setMessage] = useState('')
+  const [sendingMessage, setSendingMessage] = useState(false)
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        keyboardAppearance="dark"
+        placeholder="Qual a sua expectativa para o evento?"
+        placeholderTextColor={COLORS.GRAY_PRIMARY}
+        multiline
+        maxLength={140}
+        value={message}
+        onChangeText={setMessage}
+        editable={!sendingMessage}
+      />
+      <Button
+        title="Enviar mensagem"
+        backgroundColor={COLORS.PINK}
+        color={COLORS.WHITE}
+      />
+    </View>
+  )
+}
